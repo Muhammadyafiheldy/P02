86df064fd97fa941291c57bd5aac1050961af7f5
Muhammadyafiheldy <m.yafiheldy@gmail.com>
2025-03-10 11:08:33 +0700

    push submit asignment

---
+++ ex3_comment.dart
@@ -0,0 +1,12 @@
+//  contoh komentar
+void main (List<String>args ){
+  // komentar satu baris
+  // ini komentar kedua
+  /*
+  ** komentar lebih dari 2 baris
+  ** ok
+  */
+  /// Komentar untuk dokumentasi
+  ///
+  /// Komentar ini diguakan untuk mendokumentasi sintaks program
+}
---
+++ ex4_indentifier.dart
@@ -0,0 +1,12 @@
+//  pengenal (identifier)
+// void main(List<String> args){
+//   String nama depan;
+//   double 2dimensi;
+//   var tanda# = '#';
+// }
+
+void main(List<String> args) {
+  String namaDepan;
+  double duaDimensi;
+  var tanda = '#';
+}
